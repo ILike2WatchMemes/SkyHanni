fe5aa074f969c97625b02399cfb1869de81efbe4
HacktheTime <l4bg0jb7@duck.com>
2025-09-01 10:58:12 +0200

    added chat prompt debug code

--- src/main/java/at/hannibal2/skyhanni/config/features/chat/ChatPromptUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/chat/ChatPromptUtils.kt
@@ -1,11 +1,14 @@
 package at.hannibal2.skyhanni.config.features.chat
 
+import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.config.core.config.KeyBind
 import at.hannibal2.skyhanni.events.minecraft.KeyDownEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.DelayedRun
 import at.hannibal2.skyhanni.utils.KeyboardManager.isKeyClicked
+import at.hannibal2.skyhanni.utils.KeyboardManager.isKeyHeld
 
 @SkyHanniModule
 object ChatPromptUtils {
@@ -15,7 +18,10 @@ object ChatPromptUtils {
     @HandleEvent
     fun key(event: KeyDownEvent) {
         val activePrompt = activePromptBlock ?: return
-        if (event.keyCode != activePrompt.keyCode || !event.keyCode.isKeyClicked()) return
+        if (event.keyCode != activePrompt.keyCode || !event.keyCode.isKeyHeld()) return
+        if (SkyHanniMod.feature.dev.debug.enabled){
+            ChatUtils.chat("Chat Prompt reset")
+        }
         activePromptBlock = null
         activePrompt.codeBlock.invoke()
     }
@@ -34,8 +40,14 @@ object ChatPromptUtils {
             codeBlock = codeBlock,
         )
         this.activePromptBlock = activePromptBlock
+        if (SkyHanniMod.feature.dev.debug.enabled){
+            ChatUtils.chat("Set new Chat Prompt task")
+        }
         DelayedRun.runDelayed(keyBind.getEffectiveExpirationDuration()) {
             if (this.activePromptBlock == activePromptBlock) this.activePromptBlock = null
+            if (SkyHanniMod.feature.dev.debug.enabled){
+                ChatUtils.chat("Chat Prompt expired")
+            }
         }
 
     }
