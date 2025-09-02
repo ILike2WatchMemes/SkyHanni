ea9817107ba7ddb64e4302ff72f2d8b3ef3c582b
HacktheTime <l4bg0jb7@duck.com>
2025-09-02 17:18:22 +0200

    fixed npc chat prompts

--- src/main/java/at/hannibal2/skyhanni/config/features/chat/NPCResponseSuggestion.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/chat/NPCResponseSuggestion.kt
@@ -3,26 +3,38 @@ package at.hannibal2.skyhanni.config.features.chat
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.data.hypixel.chat.event.NpcChatEvent
+import at.hannibal2.skyhanni.events.chat.SkyHanniChatEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.HypixelCommands
 import at.hannibal2.skyhanni.utils.compat.command
+import kotlinx.coroutines.delay
 
 @SkyHanniModule
 object NPCResponseSuggestion {
+    val commandPrefixes = listOf("/selectnpcoption","/chatprompt")
+
     @HandleEvent
-    fun onMessage(event: NpcChatEvent) {
-        for (components in event.messageComponent.sampleComponents()) {
-            val command = components.command ?: continue
-            if (command.startsWith("chatprompt") && components.formattedText.contains("§a")) {
-                ChatUtils.chatPrompt(
-                    "Press §a%KEY%§e to respond with \"${components.formattedText}\"", SkyHanniMod.feature.chat.npcResponseSuggestion,
-                    {
-                        HypixelCommands.chatPrompt(command.substringAfter(" "))
-                    },
-                )
-                return
+    fun onMessage(event: SkyHanniChatEvent) {
+        SkyHanniMod.launchCoroutine {
+            val validComponents = event.chatComponent.siblings.filter { sib ->
+                commandPrefixes.any { prefix ->
+                    sib.command?.startsWith(prefix)
+                        ?: false
+                }
+            }.let {
+                val filtered = it.filter { it.formattedText.contains("§a") }
+                return@let filtered.ifEmpty { it }
             }
+            if (validComponents.isEmpty()) return@launchCoroutine
+            val components = validComponents.first()
+            val command = components.command ?: return@launchCoroutine
+            ChatUtils.chatPrompt(
+                "Press §a%KEY%§e to respond with \"${components.formattedText}\"", SkyHanniMod.feature.chat.npcResponseSuggestion,
+                {
+                    ChatUtils.sendMessageToServer(command)
+                },
+            )
         }
     }
 }
